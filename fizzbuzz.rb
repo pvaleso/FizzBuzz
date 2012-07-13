4a893094e26bd26153689038577b28d9003e618e
unknown <operador@.upc.edu.pe>
2012-07-12 21:37:31 -0500

    FizzBuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,18 @@ class FizzBuzz
 
   def numero
   	
+    if @numero%15 ==0
+  		"FizzBuzz"
+  	end
+
   	if @numero%3 ==0
   		"Fizz"
   	end
+
+  	if @numero%5 ==0
+  		"Buzz"
+  	end
+
   	@numero
   end
 
